78598e2ea2103103bea6825b720be08535a9030d
yabikuhirohumi <e195762@ie.u-ryukyu.ac.jp>
2020-01-13 16:33:27 +0900

    a

--- src/test/jp/ac/uryukyu/ie/e195762/EnemyTest.java
+++ src/test/jp/ac/uryukyu/ie/e195762/EnemyTest.java
@@ -12,10 +12,10 @@ public class EnemyTest {
         int heroHP = 10;
         Hero hero = new Hero("テスト勇者", heroHP, 5);
         Enemy enemy = new Enemy("テストスライム", 6, 3);
-        enemy.dead = true;
+        enemy.setIsDead(true);
         for (int i = 0; i < 10; i++) {
             enemy.attack(hero); //乱数で0ダメージとなることもあるため、複数回実行してみる。
         }
-        assertEquals(heroHP, hero.hitPoint);
+        assertEquals(heroHP, hero.getHitPoint());
     }
 }
